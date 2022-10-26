f67a70a4de322423c0c7f9438836445b8e066ca4
ma-shaoyin <mashaoyin1@huawei.com>
2022-10-26 20:46:59 +0800

    Signed-off-by: ma-shaoyin <mashaoyin1@huawei.com>
     Changes to be committed:

--- frameworks/inputmethod_controller/include/input_method_property.h
+++ frameworks/inputmethod_controller/include/input_method_property.h
@@ -26,7 +26,7 @@ struct Property {
     std::string id;
     std::string label;
     std::string icon;
-    int32_t iconId;
+    int32_t iconId = 0;
 };
 
 struct SubProperty {
@@ -37,7 +37,7 @@ struct SubProperty {
     std::string locale;
     std::string language;
     std::string icon;
-    int32_t iconId;
+    int32_t iconId = 0;
 };
 } // namespace MiscServices
 } // namespace OHOS
