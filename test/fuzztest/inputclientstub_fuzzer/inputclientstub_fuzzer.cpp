4b25d601b9d4ea1c1cd299dad39073651c64601e
Hollokin <taoyuxin2@huawei.com>
2022-11-15 16:40:36 +0800

    【FUZZ】ability及controller相关的FUZZ用例补充
    Signed-off-by: Hollokin <taoyuxin2@huawei.com>

--- test/fuzztest/inputclientstub_fuzzer/inputclientstub_fuzzer.cpp
+++ test/fuzztest/inputclientstub_fuzzer/inputclientstub_fuzzer.cpp
@@ -13,57 +13,97 @@
  * limitations under the License.
  */
 
-
 #include "inputclientstub_fuzzer.h"
 
 #include <cstddef>
 #include <cstdint>
+#include <input_method_agent_stub.h>
 
-#include "input_client_stub.h"
 #include "global.h"
-
+#include "input_client_stub.h"
 #include "message_parcel.h"
 
 using namespace OHOS::MiscServices;
 namespace OHOS {
-    constexpr size_t THRESHOLD = 10;
-    constexpr int32_t OFFSET = 4;
-    const std::u16string INPUTCLIENTSTUB_INTERFACE_TOKEN = u"ohos.miscservices.inputmethod.InputClient";
+constexpr size_t THRESHOLD = 10;
+constexpr int32_t OFFSET = 4;
+const std::u16string INPUTCLIENTSTUB_INTERFACE_TOKEN = u"ohos.miscservices.inputmethod.InputClient";
 
-    uint32_t ConvertToUint32(const uint8_t *ptr)
-    {
-        if (ptr == nullptr) {
-            return 0;
-        }
-        uint32_t bigVar = (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | (ptr[3]);
-        return bigVar;
+uint32_t ConvertToUint32(const uint8_t *ptr)
+{
+    if (ptr == nullptr) {
+        return 0;
     }
-    bool FuzzInputClientStub(const uint8_t* rawData, size_t size)
-    {
-        uint32_t code = ConvertToUint32(rawData);
-        rawData = rawData + OFFSET;
-        size = size - OFFSET;
-
-        MessageParcel data;
-        data.WriteInterfaceToken(INPUTCLIENTSTUB_INTERFACE_TOKEN);
-        data.WriteBuffer(rawData, size);
-        data.RewindRead(0);
-        MessageParcel reply;
-        MessageOption option;
-
-        sptr<InputClientStub> mClient = new InputClientStub;
-        mClient->OnRemoteRequest(code, data, reply, option);
-
-        return true;
-    }
+    uint32_t bigVar = (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | (ptr[3]);
+    return bigVar;
 }
+
+void FuzzInputClientStub(const uint8_t *rawData, size_t size)
+{
+    uint32_t code = ConvertToUint32(rawData);
+    rawData = rawData + OFFSET;
+    size = size - OFFSET;
+
+    MessageParcel data;
+    data.WriteInterfaceToken(INPUTCLIENTSTUB_INTERFACE_TOKEN);
+    data.WriteBuffer(rawData, size);
+    data.RewindRead(0);
+    MessageParcel reply;
+    MessageOption option;
+
+    sptr<InputClientStub> mClient = new InputClientStub();
+    MessageHandler *handler = MessageHandler::Instance();
+    mClient->SetHandler(handler);
+    mClient->OnRemoteRequest(code, data, reply, option);
+}
+
+void TextOnInputReady()
+{
+    sptr<InputClientStub> mClient = new InputClientStub();
+    sptr<InputMethodAgentStub> mInputMethodAgentStub = new InputMethodAgentStub();
+    MessageParcel data;
+    data.WriteRemoteObject(mInputMethodAgentStub->AsObject());
+    auto remoteObject = data.ReadRemoteObject();
+    sptr<IInputMethodAgent> iface = iface_cast<IInputMethodAgent>(remoteObject);
+    mClient->onInputReady(iface);
+}
+
+void TestOnInputReleased(int32_t fuzzedInt32)
+{
+    sptr<InputClientStub> mClient = new InputClientStub();
+    mClient->onInputReleased(fuzzedInt32);
+}
+
+void TestSetDisplayMode(int32_t fuzzedInt32)
+{
+    sptr<InputClientStub> mClient = new InputClientStub();
+    mClient->setDisplayMode(fuzzedInt32);
+}
+
+void TestOnSwitchInput()
+{
+    sptr<InputClientStub> mClient = new InputClientStub();
+    Property property = {};
+    SubProperty subProperty = {};
+
+    mClient->OnSwitchInput(property, subProperty);
+}
+} // namespace OHOS
+
 /* Fuzzer entry point */
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     if (size < OHOS::THRESHOLD) {
         return 0;
     }
     /* Run your code on data */
+    auto fuzzedInt32 = static_cast<int32_t>(size);
+    std::string fuzzedString(reinterpret_cast<const char *>(data), size);
+
     OHOS::FuzzInputClientStub(data, size);
+    OHOS::TextOnInputReady();
+    OHOS::TestOnInputReleased(fuzzedInt32);
+    OHOS::TestSetDisplayMode(fuzzedInt32);
+    OHOS::TestOnSwitchInput();
     return 0;
 }
