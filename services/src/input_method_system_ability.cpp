c34a126baf83cbc6aadeb3035602e016d2726aba
cy7717 <chenyu301@huawei.com>
2023-01-18 15:33:05 +0800

    修改用户切换引起的包移除的处理
    Signed-off-by: cy7717 <chenyu301@huawei.com>

--- services/src/input_method_system_ability.cpp
+++ services/src/input_method_system_ability.cpp
@@ -973,7 +973,10 @@ int32_t InputMethodSystemAbility::OnPackageRemoved(const Message *msg)
         IMSA_HILOGE("Failed to read message parcel");
         return ErrorCode::ERROR_EX_PARCELABLE;
     }
-
+    // 用户切换时也会有该通知，如果移除的app用户不是当前用户，则不处理
+    if (userId != userId_) {
+        return ErrorCode::NO_ERROR;
+    }
     auto cfg = ImeCfgManager::GetInstance().GetImeCfg(userId);
     auto &currentIme = cfg.currentIme;
     if (currentIme.empty()) {
@@ -993,7 +996,7 @@ int32_t InputMethodSystemAbility::OnPackageRemoved(const Message *msg)
             OnSwitchInputMethod(defaultIme.substr(0, pos), defaultIme.substr(pos + 1, defaultIme.length() - pos - 1));
         IMSA_HILOGI("InputMethodSystemAbility::OnPackageRemoved ret = %{public}d", ret);
     }
-    return 0;
+    return ErrorCode::NO_ERROR;
 }
     int32_t InputMethodSystemAbility::OnDisplayOptionalInputMethod(int32_t userId)
     {
