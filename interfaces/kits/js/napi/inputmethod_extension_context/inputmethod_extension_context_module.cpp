2b66a709b95ca3fca170a3187b0baec5faf549bf
openharmony_ci <120357966@qq.com>
2023-02-03 03:09:24 +0000

    回退 'Pull Request !566 : 【monthly】修改.nm_modname的名字与模块名保持一致'

--- interfaces/kits/js/napi/inputmethod_extension_context/inputmethod_extension_context_module.cpp
+++ interfaces/kits/js/napi/inputmethod_extension_context/inputmethod_extension_context_module.cpp
@@ -20,17 +20,17 @@ extern const char _binary_inputmethod_extension_context_js_end[];
 extern const char _binary_inputmethod_extension_context_abc_start[];
 extern const char _binary_inputmethod_extension_context_abc_end[];
 
-extern "C" __attribute__((constructor)) void NAPI_InputMethodExtensionContext_AutoRegister()
+extern "C" __attribute__((constructor)) void NAPI_inputmethodextensioncontext_AutoRegister()
 {
     auto moduleManager = NativeModuleManager::GetInstance();
     NativeModule newModuleInfo = {
-        .name = "InputMethodExtensionContext",
+        .name = "inputmethodextensioncontext",
         .fileName = "libinputmethodextensioncontext_napi.so/inputmethod_extension_context.js",
     };
     moduleManager->Register(&newModuleInfo);
 }
 
-extern "C" __attribute__((visibility("default"))) void NAPI_InputMethodExtensionContext_GetJSCode(
+extern "C" __attribute__((visibility("default"))) void NAPI_inputmethodextensioncontext_GetJSCode(
     const char **buf, int *bufLen)
 {
     if (buf != nullptr) {
@@ -42,7 +42,7 @@ extern "C" __attribute__((visibility("default"))) void NAPI_InputMethodExtension
 }
 
 // ability_context JS register
-extern "C" __attribute__((visibility("default"))) void NAPI_InputMethodExtensionContext_GetABCCode(
+extern "C" __attribute__((visibility("default"))) void NAPI_inputmethodextensioncontext_GetABCCode(
     const char **buf, int *buflen)
 {
     if (buf != nullptr) {
--- interfaces/kits/js/napi/inputmethodclient/input_method_module.cpp
+++ interfaces/kits/js/napi/inputmethodclient/input_method_module.cpp
@@ -37,7 +37,7 @@ static napi_module _module = {
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
-    .nm_modname = "inputMethod",
+    .nm_modname = "inputmethod",
     .nm_priv = ((void *)0),
     .reserved = {0}
 };
