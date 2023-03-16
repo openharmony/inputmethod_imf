7bddfadffa94b97fbb235eae30f37c4f3b4d4422
ma-shaoyin <mashaoyin1@huawei.com>
2023-03-16 17:36:13 +0800

    Signed-off-by: ma-shaoyin <mashaoyin1@huawei.com>
     Changes to be committed:

--- frameworks/native/inputmethod_ability/include/input_method_agent_proxy.h
+++ frameworks/native/inputmethod_ability/include/input_method_agent_proxy.h
@@ -44,6 +44,8 @@ public:
 
 private:
     static inline BrokerDelegator<InputMethodAgentProxy> delegator_;
+    using ParcelHandler = std::function<bool(MessageParcel &)>;
+    int32_t SendRequest(int code, ParcelHandler input = nullptr, ParcelHandler output = nullptr);
 };
 } // namespace MiscServices
 } // namespace OHOS
