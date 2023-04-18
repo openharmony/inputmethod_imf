f9b9bea7019e9ed14d40c956fbbfac83448b3c9f
Hollokin <taoyuxin2@huawei.com>
2023-04-18 16:50:59 +0800

    格式化
    Signed-off-by: Hollokin <taoyuxin2@huawei.com>

--- frameworks/native/inputmethod_ability/include/input_method_panel.h
+++ frameworks/native/inputmethod_ability/include/input_method_panel.h
@@ -48,7 +48,8 @@ class InputMethodPanel {
 public:
     InputMethodPanel() = default;
     ~InputMethodPanel();
-    int32_t SetUiContent(const std::string &contentInfo, NativeEngine &engine, std::shared_ptr<NativeReference> &contentStorage);
+    int32_t SetUiContent(const std::string &contentInfo, NativeEngine &engine,
+        std::shared_ptr<NativeReference> &contentStorage);
     int32_t CreatePanel(const std::shared_ptr<AbilityRuntime::Context> &context, const PanelInfo &panelInfo);
     int32_t DestroyPanel();
 
