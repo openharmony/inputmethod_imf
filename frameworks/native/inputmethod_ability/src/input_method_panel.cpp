6499c4c7dcda1d19491c60134e7af889a9f294b8
cy7717 <chenyu301@huawei.com>
2023-05-06 14:16:17 +0800

    扩展name为 name+/+id
    Signed-off-by: cy7717 <chenyu301@huawei.com>

--- frameworks/native/inputmethod_ability/src/input_method_panel.cpp
+++ frameworks/native/inputmethod_ability/src/input_method_panel.cpp
@@ -204,10 +204,10 @@ void InputMethodPanel::PanelStatusChange(const InputWindowStatus &status)
     auto imsa = ImaUtils::GetImsaProxy();
     if (imsa != nullptr && panelType_ == SOFT_KEYBOARD && panelFlag_ == FLG_FIXED) {
         auto rect = window_->GetRect();
-        IMSA_HILOGD("InputMethodPanel::rect, x:%{public}d, y:%{public}d, w:%{public}d, h:%{public}d", rect.posX_,
-            rect.posY_, rect.width_, rect.height_);
-        imsa->PanelStatusChange(
-            status, { window_->GetWindowName(), rect.posX_, rect.posY_, rect.width_, rect.height_ });
+        IMSA_HILOGD("InputMethodPanel::x:%{public}d, y:%{public}d, w:%{public}d, h:%{public}d", rect.posX_, rect.posY_,
+            rect.width_, rect.height_);
+        std::string name = window_->GetWindowName() + "/" + std::to_string(window_->GetWindowId());
+        imsa->PanelStatusChange(status, { std::move(name), rect.posX_, rect.posY_, rect.width_, rect.height_ });
     }
 }
 
