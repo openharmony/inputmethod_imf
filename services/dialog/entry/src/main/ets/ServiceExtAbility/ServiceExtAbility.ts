4a447a72ab5eb64eba9a04ce6d482ab822be9dd0
ma-shaoyin <mashaoyin1@huawei.com>
2023-05-24 19:45:33 +0800

    Signed-off-by: ma-shaoyin <mashaoyin1@huawei.com>
     Changes to be committed:

--- services/dialog/entry/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts
+++ services/dialog/entry/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts
@@ -70,6 +70,9 @@ export default class ServiceExtAbility extends ServiceExtensionAbility {
     console.log(TAG + 'createWindow execute');
     try {
       if (globalThis.windowNum > 0) {
+        globalThis.windowNum = 0;
+        globalThis.extensionWin.destroy();
+        globalThis.context.terminateSelf();
         return;
       }
       const win = await window.create(this.context, name, windowType);
