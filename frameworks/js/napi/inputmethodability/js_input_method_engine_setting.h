ac016a77b06a652a4acbfd1fe54677842692ace7
Hollokin <taoyuxin2@huawei.com>
2023-06-01 15:17:19 +0800

    修改异常抛出的捕获问题
    Signed-off-by: Hollokin <taoyuxin2@huawei.com>

--- frameworks/js/napi/inputmethodability/js_input_method_engine_setting.h
+++ frameworks/js/napi/inputmethodability/js_input_method_engine_setting.h
@@ -61,7 +61,7 @@ private:
 
         napi_status operator()(napi_env env, size_t argc, napi_value *argv, napi_value self) override
         {
-            NAPI_ASSERT_BASE(env, self != nullptr, "self is nullptr", napi_invalid_arg);
+            CHECK_RETURN(self != nullptr, "self is nullptr", napi_invalid_arg);
             return Context::operator()(env, argc, argv, self);
         }
         napi_status operator()(napi_env env, napi_value *result) override
