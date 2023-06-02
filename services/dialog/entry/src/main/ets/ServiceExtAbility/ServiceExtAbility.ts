a310fa0fc6563a37b8166a20044923d6234672df
mashaoyin <mashaoyin1@huawei.com>
2023-06-02 05:02:04 +0000

    update services/dialog/entry/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts.
    Signed-off-by: mashaoyin <mashaoyin1@huawei.com>

--- services/dialog/entry/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts
+++ services/dialog/entry/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts
@@ -51,7 +51,7 @@ export default class ServiceExtAbility extends ServiceExtensionAbility {
 
     commonEvent.createSubscriber(subscribeInfo, (error, subcriber) => {
       commonEvent.subscribe(subcriber, (error, commonEventData) => {
-        if (commonEventData.event === commonEvent1 && commonEventData.event === commonEvent2) {
+        if (commonEventData.event === commonEvent1 || commonEventData.event === commonEvent2) {
           console.log(TAG + 'commonEvent:' + JSON.stringify(commonEvent1));
           this.getInputMethods().then(() => {
             if (!globalThis.extensionWin.isWindowShowing()) {
