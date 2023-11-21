e66ea28986e7ad367706d9123ce0b9eae3bb54a5
h30051954 <hemenghao2@huawei.com>
2023-11-21 11:14:21 +0800

    close耗时整改
    Signed-off-by: h30051954 <hemenghao2@huawei.com>

--- frameworks/native/inputmethod_ability/src/input_method_core_stub.cpp
+++ frameworks/native/inputmethod_ability/src/input_method_core_stub.cpp
@@ -129,7 +129,7 @@ int32_t InputMethodCoreStub::StopInputOnRemote(MessageParcel &data, MessageParce
 {
     IMSA_HILOGD("InputMethodCoreStub::StopInputOnRemote");
     sptr<IRemoteObject> channelObject = nullptr;
-    int32_t ret = SendMessage(MessageID::MSG_ID_SET_SUBTYPE, [&data, &channelObject](MessageParcel &parcel) {
+    int32_t ret = SendMessage(MessageID::MSG_ID_STOP_INPUT, [&data, &channelObject](MessageParcel &parcel) {
         return ITypesUtil::Unmarshal(data, channelObject) && ITypesUtil::Marshal(parcel, channelObject);
     });
     return reply.WriteInt32(ret) ? ErrorCode::NO_ERROR : ErrorCode::ERROR_EX_PARCELABLE;
