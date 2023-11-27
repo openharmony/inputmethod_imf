3af39bd37765832681e12355dab19330bab326af
cy7717 <chenyu301@huawei.com>
2023-11-27 15:50:22 +0800

    mod
    Signed-off-by: cy7717 <chenyu301@huawei.com>

--- frameworks/js/napi/inputmethodability/js_input_method_engine_setting.cpp
+++ frameworks/js/napi/inputmethodability/js_input_method_engine_setting.cpp
@@ -780,7 +780,7 @@ std::shared_ptr<AppExecFwk::EventHandler> JsInputMethodEngineSetting::GetEventHa
 std::shared_ptr<JsInputMethodEngineSetting::UvEntry> JsInputMethodEngineSetting::GetEntry(
     const std::string &type, EntrySetter entrySetter)
 {
-    IMSA_HILOGD("run in, type: %{public}s", type.c_str());
+    IMSA_HILOGD("type: %{public}s", type.c_str());
     std::shared_ptr<UvEntry> entry = nullptr;
     {
         std::lock_guard<std::recursive_mutex> lock(mutex_);
