40aec6fd7f9a1de6f2d9451bea72ac64697d77cc
zhaolinglan <zhaolinglan@huawei.com>
2024-01-29 16:55:56 +0800

    connect ability
    Signed-off-by: zhaolinglan <zhaolinglan@huawei.com>

--- services/src/peruser_session.cpp
+++ services/src/peruser_session.cpp
@@ -21,6 +21,7 @@
 #include "element_name.h"
 #include "ime_aging_manager.h"
 #include "ime_cfg_manager.h"
+#include "ime_connection.h"
 #include "ime_info_inquirer.h"
 #include "input_client_proxy.h"
 #include "input_control_channel_proxy.h"
@@ -940,19 +941,24 @@ void PerUserSession::DeactivateIme(const std::string &bundleName, const std::str
         IMSA_HILOGE("ime: %{public}d is not exist", ImeType::IME);
         return;
     }
-    IMSA_HILOGI("ime: %{public}s/%{public}s", bundleName.c_str(), subName.c_str());
-    data->deathRecipient->SetDeathRecipient(
-        [this, bundleName](const wptr<IRemoteObject> &) { ImeAgingManager::GetInstance().Pop(bundleName); });
+    auto info = ImeInfoInquirer::GetInstance().GetImeInfo(userId_, bundleName, subName);
+    if ((info == nullptr || !info->isNewIme) && data->core != nullptr) {
+        IMSA_HILOGI("stop ime: %{public}s/%{public}s", bundleName.c_str(), subName.c_str());
+        data->core->StopInputService(true);
+        RemoveImeData(ImeType::IME, true);
+        return;
+    }
+
+    IMSA_HILOGI("deactivate ime: %{public}s/%{public}s", bundleName.c_str(), subName.c_str());
+    if (data->deathRecipient != nullptr) {
+        data->deathRecipient->SetDeathRecipient(
+            [this, bundleName](const wptr<IRemoteObject> &) { ImeAgingManager::GetInstance().Pop(bundleName); });
+    }
     ImeAgingManager::GetInstance().Push(bundleName, data);
     auto client = GetCurrentClient();
     auto clientInfo = client != nullptr ? GetClientInfo(client->AsObject()) : nullptr;
     if (clientInfo != nullptr && clientInfo->bindImeType == ImeType::IME) {
         UnBindClientWithIme(clientInfo, false);
-    }
-    auto info = ImeInfoInquirer::GetInstance().GetImeInfo(userId_, bundleName, subName);
-    if (info != nullptr && !info->isNewIme && data->core != nullptr) {
-        IMSA_HILOGD("old ime, need to StopInputService");
-        data->core->StopInputService(false);
     }
     RemoveImeData(ImeType::IME, false);
 }
@@ -963,8 +969,12 @@ bool PerUserSession::StartInputService(const std::shared_ptr<ImeNativeCfg> &ime,
     AAFwk::Want want;
     want.SetElementName(ime->bundleName, ime->extName);
     isImeStarted_.Clear(false);
-    auto ret = AAFwk::AbilityManagerClient::GetInstance()->StartExtensionAbility(
-        want, nullptr, userId_, AppExecFwk::ExtensionAbilityType::INPUTMETHOD);
+    auto connection = new (std::nothrow) ImeConnection();
+    if (connection == nullptr) {
+        IMSA_HILOGE("failed to create connection");
+        return false;
+    }
+    auto ret = AAFwk::AbilityManagerClient::GetInstance()->ConnectExtensionAbility(want, connection, userId_);
     if (ret != ErrorCode::NO_ERROR) {
         IMSA_HILOGE("failed to start ability");
         InputMethodSysEvent::GetInstance().InputmethodFaultReporter(
