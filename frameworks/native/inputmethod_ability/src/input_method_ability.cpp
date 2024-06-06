3ae0e9be6c5d5026c2feb40fcd04498ae8f20257
ma-shaoyin <mashaoyin1@huawei.com>
2024-06-06 16:52:59 +0800

    Signed-off-by: ma-shaoyin <mashaoyin1@huawei.com>
     Changes to be committed:

--- frameworks/native/inputmethod_ability/src/input_method_ability.cpp
+++ frameworks/native/inputmethod_ability/src/input_method_ability.cpp
@@ -826,8 +826,8 @@ int32_t InputMethodAbility::CreatePanel(const std::shared_ptr<AbilityRuntime::Co
     auto flag = panels_.ComputeIfAbsent(
         panelInfo.panelType, [panelHeightCallback, &panelInfo, &context, &inputMethodPanel](
                                  const PanelType &panelType, std::shared_ptr<InputMethodPanel> &panel) {
-            inputMethodPanel->SetPanelHeightCallback(panelHeightCallback);
             inputMethodPanel = std::make_shared<InputMethodPanel>();
+            inputMethodPanel->SetPanelHeightCallback(panelHeightCallback);
             auto ret = inputMethodPanel->CreatePanel(context, panelInfo);
             if (ret == ErrorCode::NO_ERROR) {
                 panel = inputMethodPanel;
