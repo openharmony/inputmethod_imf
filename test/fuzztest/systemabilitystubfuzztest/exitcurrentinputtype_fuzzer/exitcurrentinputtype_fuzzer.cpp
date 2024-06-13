ece6936c365459b372830f66b47199c61a819d53
h30051954 <hemenghao2@huawei.com>
2024-06-13 22:01:26 +0800

    fuzz覆盖率
    Signed-off-by: h30051954 <hemenghao2@huawei.com>

---
+++ test/fuzztest/systemabilitystubfuzztest/exitcurrentinputtype_fuzzer/exitcurrentinputtype_fuzzer.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "exitcurrentinputtype_fuzzer.h"
+#include "imf_sa_stub_fuzz_util.h"
+#include "inputmethod_service_ipc_interface_code.h"
+
+using namespace OHOS::MiscServices;
+namespace OHOS {
+} // namespace OHOS
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    ImfSaStubFuzzUtil::FuzzInputMethodSystemAbility(data, size,
+        InputMethodInterfaceCode::EXIT_CURRENT_INPUT_TYPE);
+    return 0;
+}
