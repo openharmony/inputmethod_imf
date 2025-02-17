536db27a991105264d0518ad8f145561592d1488
zhaolinglan <zhaolinglan1@huawei.com>
2025-02-17 17:17:16 +0800

    change adjust to public
    Signed-off-by: zhaolinglan <zhaolinglan1@huawei.com>

--- test/unittest/cpp_test/src/input_method_panel_test.cpp
+++ test/unittest/cpp_test/src/input_method_panel_test.cpp
@@ -107,7 +107,10 @@ public:
             IMSA_HILOGI("PanelStatusListenerImpl OnPanelStatus in, isShow is %{public}s", isShow ? "true" : "false");
         }
         void OnSizeChange(uint32_t windowId, const WindowSize &size) {}
-        void OnSizeChange(uint32_t windowId, const WindowSize &size, const PanelAdjustInfo &keyboardArea) {}
+        void OnSizeChange(
+            uint32_t windowId, const WindowSize &size, const PanelAdjustInfo &keyboardArea, const std::string &event)
+        {
+        }
     };
     static std::mutex imcPanelStatusListenerLock_;
     static std::condition_variable imcPanelStatusListenerCv_;
