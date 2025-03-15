f7a7579381bfccfdce984e7a8da4425e1f788e6f
guojin31 <guojin31@huawei.com>
2025-03-15 16:05:35 +0800

    修改单手兼容模式下全屏手写问题
    Signed-off-by: guojin31 <guojin31@huawei.com>

--- frameworks/native/inputmethod_ability/src/input_method_panel.cpp
+++ frameworks/native/inputmethod_ability/src/input_method_panel.cpp
@@ -421,9 +421,18 @@ int32_t InputMethodPanel::AdjustKeyboard()
 {
     isAdjustInfoInitialized_.store(false);
     int32_t ret = 0;
-    if (!isInEnhancedAdjust_.load()) {
-        LayoutParams params = { enhancedLayoutParams_.landscape.rect, enhancedLayoutParams_.portrait.rect };
-        ret = AdjustPanelRect(panelFlag_, params);
+    if (isInEnhancedAdjust_.load()) {
+        EnhancedLayoutParams params = enhancedLayoutParams_;
+        DisplaySize displaySize;
+        ret = GetDisplaySize(displaySize);
+        if (ret != ErrorCode::NO_ERROR) {
+            IMSA_HILOGE("failed to GetDisplaySize ret: %{public}d", ret);
+            return ret;
+        }
+        params.portrait.avoidY = displaySize.portrait.height - params.portrait.avoidHeight;
+        params.landscape.avoidY = displaySize.landscape.height - params.landscape.avoidHeight;
+        auto hotAreas = GetHotAreas();
+        ret = AdjustPanelRect(panelFlag_, params, hotAreas);
     } else {
         LayoutParams params = { enhancedLayoutParams_.landscape.rect, enhancedLayoutParams_.portrait.rect };
         ret = AdjustPanelRect(panelFlag_, params);
