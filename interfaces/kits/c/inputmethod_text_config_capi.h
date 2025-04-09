68b6d9b6c34d8edf6fe9e34a22798c862754913d
qianyong325 <qianyong15@h-partners.com>
2025-04-09 18:28:37 +0800

    修改checkout
    Signed-off-by: qianyong325 <qianyong15@h-partners.com>

--- interfaces/kits/c/inputmethod_text_config_capi.h
+++ interfaces/kits/c/inputmethod_text_config_capi.h
@@ -146,7 +146,7 @@ InputMethod_ErrorCode OH_TextConfig_SetWindowId(InputMethod_TextConfig *config, 
  * Specific error codes can be referenced {@link InputMethod_ErrorCode}.
  * @since 20
  */
- InputMethod_ErrorCode OH_TextConfig_SetPlaceholder(InputMethod_TextConfig *config, const char16_t *placeholder,
+InputMethod_ErrorCode OH_TextConfig_SetPlaceholder(InputMethod_TextConfig *config, const char16_t *placeholder,
     size_t length);
 
 /**
@@ -164,7 +164,7 @@ InputMethod_ErrorCode OH_TextConfig_SetWindowId(InputMethod_TextConfig *config, 
  * Specific error codes can be referenced {@link InputMethod_ErrorCode}.
  * @since 20
  */
-InputMethod_ErrorCode OH_TextConfig_SetAbilityName(InputMethod_TextConfig *config, const char16_t *abilityName,
+ InputMethod_ErrorCode OH_TextConfig_SetAbilityName(InputMethod_TextConfig *config, const char16_t *abilityName,
     size_t length);
 
 /**
