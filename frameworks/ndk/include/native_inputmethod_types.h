162bb44ccd96508b8170402258467d84a256dc34
qianyong325 <qianyong15@h-partners.com>
2025-04-21 06:04:12 +0000

    update frameworks/ndk/include/native_inputmethod_types.h.
    Signed-off-by: qianyong325 <qianyong15@h-partners.com>

--- frameworks/ndk/include/native_inputmethod_types.h
+++ frameworks/ndk/include/native_inputmethod_types.h
@@ -39,7 +39,6 @@ struct InputMethod_TextAvoidInfo {
     double positionY;
     double height;
 };
-
 struct InputMethod_TextConfig {
     InputMethod_TextInputType inputType;
     InputMethod_EnterKeyType enterKeyType;
