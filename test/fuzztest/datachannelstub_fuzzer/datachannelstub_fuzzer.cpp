5d144554b493ff6b9f756717e431ba2f1454c7d4
qianyong325 <qianyong15@h-partners.com>
2025-04-22 19:42:25 +0800

    fix fuzz
    Signed-off-by: qianyong325 <qianyong15@h-partners.com>

--- test/fuzztest/datachannelstub_fuzzer/datachannelstub_fuzzer.cpp
+++ test/fuzztest/datachannelstub_fuzzer/datachannelstub_fuzzer.cpp
@@ -26,7 +26,7 @@ using namespace OHOS::MiscServices;
 namespace OHOS {
 constexpr size_t THRESHOLD = 10;
 constexpr int32_t OFFSET = 4;
-const std::u16string DATACHANNEL_INTERFACE_TOKEN = u"ohos.miscservices.inputmethod.IInputDataChannel";
+const std::u16string DATACHANNEL_INTERFACE_TOKEN = u"OHOS.MiscServices.IInputDataChannel";
 
 uint32_t ConvertToUint32(const uint8_t *ptr)
 {
