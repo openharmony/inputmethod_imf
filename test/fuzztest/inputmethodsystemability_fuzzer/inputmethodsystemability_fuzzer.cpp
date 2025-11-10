a4b9a33ec5f70569952ec1cf08a534f8f898308c
cbloooong <cbloooong@noreply.gitcode.com>
2025-11-10 15:29:44 +0800

    add fuzz
    Signed-off-by: blc <caobaolong5@huawei.com>

--- test/fuzztest/inputmethodsystemability_fuzzer/inputmethodsystemability_fuzzer.cpp
+++ test/fuzztest/inputmethodsystemability_fuzzer/inputmethodsystemability_fuzzer.cpp
@@ -95,6 +95,14 @@ void SystemAbility(const uint8_t *data, size_t size)
     auto fuzzedUint32 = provider.ConsumeIntegral<uint32_t>();
     DelayedSingleton<InputMethodSystemAbility>::GetInstance()->ReleaseInput(nullptr, fuzzedUint32);
 }
+
+void FuzzInputType(FuzzedDataProvider &provider)
+{
+    const int32_t type = provider.ConsumeIntegral<int32_t>();
+    DelayedSingleton<InputMethodSystemAbility>::GetInstance()->StartInputType(type);
+    bool resultValue = provider.ConsumeBool();
+    DelayedSingleton<InputMethodSystemAbility>::GetInstance()->IsInputTypeSupported(type, resultValue);
+}
 } // namespace OHOS
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
@@ -107,5 +115,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     OHOS::FuzzOnUser(userId, fuzzedString);
     OHOS::FuzzOnScreenUnlock();
     OHOS::SystemAbility(data, size);
+    OHOS::FuzzInputType(provider);
     return 0;
 }
