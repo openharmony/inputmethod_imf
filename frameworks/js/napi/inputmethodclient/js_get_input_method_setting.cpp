90e1618eed3b4062d8988d5dfed772d1302e7b18
KangPeng <kangpeng18@h-partners.com>
2025-12-11 15:24:21 +0800

    输入法 arkts API异常优化
    Signed-off-by: KangPeng <kangpeng18@h-partners.com>

--- frameworks/js/napi/inputmethodclient/js_get_input_method_setting.cpp
+++ frameworks/js/napi/inputmethodclient/js_get_input_method_setting.cpp
@@ -74,7 +74,7 @@ napi_value JsGetInputMethodSetting::Init(napi_env env, napi_value exports)
     napi_value cons = nullptr;
     IMF_CALL(napi_define_class(env, IMS_CLASS_NAME.c_str(), IMS_CLASS_NAME.size(), JsConstructor, nullptr,
                                sizeof(properties) / sizeof(napi_property_descriptor),
-                               properties, &constructor));
+                               properties, &cons));
     IMF_CALL(napi_create_reference(env, cons, 1, &IMSRef_));
     IMF_CALL(napi_set_named_property(env, exports, IMS_CLASS_NAME.c_str(), cons));
     return exports;
